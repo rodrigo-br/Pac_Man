31f5cbe7c2cdc678185b7d28b4d063622fb2296f
rodrigo-br <rodrigoab123@gmail.com>
2023-01-22 16:10:34 -0300

    first steps on Actor class

---
+++ header_files/Actor.hpp
@@ -0,0 +1,52 @@
+#ifndef ACTOR_HPP
+# define ACTOR_HPP
+
+#include "defines.hpp"
+
+class Actor
+{
+	public:
+		enum State
+		{
+			ACTIVE,
+			PAUSED,
+			DEAD
+		};
+
+/*******************************************************************************
+ * 				CONSTRUCTOR AND DESTRUCTOR									   *
+ ******************************************************************************/
+		Actor(class Game* game);
+		virtual ~Actor();
+
+		void			update(float deltaTime);
+		void			updateComponents(float deltaTime);
+		virtual void	updateActor(float deltaTime);
+
+/*******************************************************************************
+ * 				GETTERS AND SETTERS								 			   *
+ ******************************************************************************/
+	public:
+// ...
+
+/*******************************************************************************
+ * 				ADD/REMOVE COMPONENTS								 		   *
+ ******************************************************************************/
+	public:
+		void			addComponent(class Component* component);
+		void			removeComponent(class Component* component);
+
+/*******************************************************************************
+ * 				PRIVATE MEMBERS								 				   *
+ ******************************************************************************/
+	private:
+		State							_state;
+		Vec2							_position;
+		float							_scale;
+		float							_rotation;
+		class Game* 					_game;
+		std::vector<class Component*>	_components;
+
+};
+
+#endif
---
+++ header_files/defines.hpp
@@ -0,0 +1,9 @@
+#ifndef DEFINES_HPP
+# define DEFINES_HPP
+
+typedef struct Vector2 {
+	float x;
+	float y;
+} Vec2;
+
+#endif
